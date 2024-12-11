9929d7eafbaf445b81b53f87c2331de8936eecc2
William Turner <willguyturner@gmail.com>
2024-12-11 21:34:14 +1300

    Add basic blank window
    Managed to link SDL and initialise a small black window with an event loop.

--- src/raycast.c
+++ src/raycast.c
@@ -1,8 +1,44 @@
 #include <SDL2/SDL.h>
+#include <assert.h>
 #include <stdio.h>
 
-int main() {
-    printf("Hello World");
 
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+SDL_Window *window;
+uint32_t pixels[SCREEN_WIDTH * SCREEN_HEIGHT];
+int quit = 0;
+
+int main(int argc, char *argv[]) {
+    //init 
+    assert(SDL_Init(SDL_INIT_VIDEO) >= 0);
+
+    window = SDL_CreateWindow(
+        "Raycast Demo",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_ALLOW_HIGHDPI);
+    
+    assert(window >= 0);
+    // SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+    // assert (window_surface != NULL);
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Event e;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            }
+        }
+        SDL_Delay(100);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
