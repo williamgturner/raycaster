035b37c7499e973077b1a3c5c10a4a6f32a7bf76
William Turner <willguyturner@gmail.com>
2024-12-12 20:38:16 +1300

    Add cast_rays func
    Begun work on casting rays using trigonometry. Updated README.

--- README.md
+++ README.md
@@ -2,11 +2,9 @@
 A simple raycasting renderer written in C using the SDL2 library
 
 ## Dependencies
-SDL2 is required, homebrew installs to /opt/homebrew/Cellar/sdl2 however I could not get this to work so it should instead be manually installed to /Library/Frameworks
+SDL2 is required, homebrew installs to /opt/homebrew/Cellar/sdl2 however I could not get this to work so it should instead be manually installed to /Library/Frameworks. The makefile will likely need to be edited to work on Windows/Linux.
 
 ## Build Instructions
 ~~~
-make clean
-make build
-make run
+make all
 ~~~
--- src/raycast.c
+++ src/raycast.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
+#define WORLD_SCALE 64
+#define FOV 60.0
+
 SDL_Window *window;
 SDL_Renderer *renderer;
 uint32_t pixels[SCREEN_WIDTH * SCREEN_HEIGHT];
@@ -13,7 +16,7 @@ int quit = 0;
 uint8_t map[36] = {
     1, 1, 1, 1, 1, 1,
     1, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 1,
+    1, 0, 0, 2, 0, 1,
     1, 0, 0, 0, 0, 1,
     1, 0, 0, 0, 0, 1,
     1, 1, 1, 1, 1, 1,
@@ -25,6 +28,24 @@ typedef struct {
     float theta;
 } observer;
 
+observer camera = {3 * 64, 3 * 64, 0.0};
+
+void cast_rays() {
+    static float ray_delta = FOV / (float) SCREEN_WIDTH;
+    float ray_theta = (camera.theta - 30);
+    int ray_x = 0;
+    int ray_y = 0;
+
+    for (int i = 0; i < SCREEN_WIDTH; i++) {
+        ray_theta += i * ray_delta;
+        if (ray_theta >= 270 || ray_theta <= 90) {
+            ray_y = ((int) camera.y / WORLD_SCALE) * 64 + 64;
+        } else {
+            ray_y = ((int) camera.y / WORLD_SCALE) * 64 - 1;
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     //init 
     assert(SDL_Init(SDL_INIT_VIDEO) >= 0);
@@ -33,8 +54,8 @@ int main(int argc, char *argv[]) {
         "Raycast Demo",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT,
+        640,
+        480,
         SDL_WINDOW_ALLOW_HIGHDPI);
     
     assert(window >= 0);
@@ -44,7 +65,7 @@ int main(int argc, char *argv[]) {
     // SDL_Surface *window_surface = SDL_GetWindowSurface(window);
     // assert (window_surface != NULL);
 
-    SDL_SetRenderDrawColor(renderer, 0xff,0x00,0x00,SDL_ALPHA_OPAQUE);
+    SDL_SetRenderDrawColor(renderer, 0xff, 0x00, 0x00, SDL_ALPHA_OPAQUE);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
@@ -56,21 +77,22 @@ int main(int argc, char *argv[]) {
             SCREEN_WIDTH,
             SCREEN_HEIGHT);
 
-    pixels[300] = 0xFF0000FF;
-    for (int y = 50; y <= 300; y++) {
+    // debug
+    for (int y = 0; y <= SCREEN_HEIGHT; y++) {
         pixels[(y * SCREEN_WIDTH) + 100] = 0xFF0000FF;
     }
 
     SDL_UpdateTexture(texture, NULL, pixels, SCREEN_WIDTH * 4);
     
     SDL_RenderCopyEx(
-            renderer,
-            texture,
-            NULL,
-            NULL,
-            0.0,
-            NULL,
-            SDL_FLIP_VERTICAL);
+        renderer,
+        texture,
+        NULL,
+        NULL,
+        0.0,
+        NULL,
+        SDL_FLIP_VERTICAL);
+
     SDL_RenderPresent(renderer);
 
     SDL_Event e;
